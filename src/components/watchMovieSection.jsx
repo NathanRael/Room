8df482f95b5cbf24e7afda94d9608bb721c70712
NathanRael <ralaivoavy.natanael@gmail.com>
2023-11-21 22:38:19 +0300

    ada

--- src/components/watchMovieSection.jsx
+++ src/components/watchMovieSection.jsx
@@ -10,6 +10,7 @@ export default function WatchMovieSection(){
                 size='32px'
             />
             <VideoScreen/>
+            
             <PlayList/>
         </section>
     )
