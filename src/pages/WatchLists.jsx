5c0719beab87f62e23eeed55ce45ade39225dcf8
NathanRael <ralaivoavy.natanael@gmail.com>
2023-12-20 13:21:02 +0300

    update watchList

--- src/pages/WatchLists.jsx
+++ src/pages/WatchLists.jsx
@@ -6,29 +6,54 @@ import { loadMovie, saveMovie } from "../functions/saveInfo";
 import { useNavigate } from "react-router-dom";
 import DataContext from "../context/DataContext";
 import Navbar from "../components/Navbar";
+import Card from "../components/Card";
 
 export default function WatchLists() {
   const { animeWatchList, setAnimeWatchList } = useContext(DataContext);
   const [watchedList, setWatchedList] = useState(animeWatchList);
   const navigate = useNavigate();
+
   let animeWatchListItem;
-  if (watchedList.length > 0) {
+
     animeWatchListItem = watchedList.map((anime) => (
-      <WatchList
-        key={anime.id}
-        srcImage={anime.attributes.posterImage.large}
-        title={anime.attributes.canonicalTitle}
-        handleclick={() => {
-          removeList(anime.id);
-          setAnimeWatchList(loadMovie("watchList") || []);
-        }}
-        onWatch={() => {
-          // saveMovie("currentMoviePlayed", anime);
-          navigate(`/Watch/${anime.attributes.slug}/${anime.id}`);
-        }}
-      />
+      <div className="col-auto" style={{maxWidth : 'max-content'}}>
+        <WatchList
+          key={anime.id}
+          srcImage={anime.attributes.posterImage.large}
+          title={anime.attributes.canonicalTitle}
+          rate={anime.attributes.averageRating}
+          status={anime.attributes.status}
+          date={anime.attributes.createdAt.slice(0, 4)}
+          handleclick={() => {
+            removeList(anime.id);
+            setAnimeWatchList(loadMovie("watchList") || []);
+          }}
+          onWatch={() => {
+            // saveMovie("currentMoviePlayed", anime);
+            navigate(`/Watch/${anime.attributes.slug}/${anime.id}`);
+          }}
+        />
+      </div>
     ));
-  }
+  
+
+  // if (watchedList?.length > 0) {
+  //   animeWatchListItem = watchedList.map((anime) => (
+  //     <WatchList
+  //       key={anime.id}
+  //       srcImage={anime.attributes.posterImage.large}
+  //       title={anime.attributes.canonicalTitle}
+  //       handleclick={() => {
+  //         removeList(anime.id);
+  //         setAnimeWatchList(loadMovie("watchList") || []);
+  //       }}
+  //       onWatch={() => {
+  //         // saveMovie("currentMoviePlayed", anime);
+  //         navigate(`/Watch/${anime.attributes.slug}/${anime.id}`);
+  //       }}
+  //     />
+  //   ));
+  //   }
 
   function removeList(id) {
     setWatchedList((prevWatchedList) => {
@@ -44,10 +69,19 @@ export default function WatchLists() {
       <div className="container-fluid">
         <SearchBar is_fixed={false} showSearchBar={false} title="Watch List" />
       </div>
-      <div className="bg-secondary container-fluid p-0 ps-md-156 pe-md-16">
-        <div className="d-flex flex-column row-gap-32 justify-content-center   pt-256 pb-128 py-md-128">
+      <div className="bg-secondary container-fluid p-0ps-sm-128 ps-lg-156 pe-sm-32">
+        {/* <div className="d-flex flex-column row-gap-32 justify-content-center   pt-256 pb-128 py-md-128">
           {watchedList.length > 0 ? (
               [animeWatchListItem]
+          ) : (
+            <p className="_lead  text-primary text-center me-md-128">
+              Empty watch list
+            </p>
+          )}
+        </div> */}
+        <div className="row d-flex flex-column row-gap-32 justify-content-center   pt-256 pb-128 py-md-128">
+          {watchedList.length > 0 ? (
+            animeWatchListItem
           ) : (
             <p className="_lead  text-primary text-center me-md-128">
               Empty watch list
